1be68411673df9284dec3c3de58c55cc499bd26c
Douglas R <douglas.srochak@gmail.com>
2024-03-23 17:13:16 -0300

    test: Add aux class to make student and question on database for tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -2,7 +2,10 @@ import { UniqueEntityID } from '@/core/entities';
 import { Question } from '@/domain/forum/enterprise/entities';
 import { QuestionProps } from '@/domain/forum/enterprise/entities/question';
 import { Slug } from '@/domain/forum/enterprise/entities/shared';
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
 
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
@@ -20,4 +23,28 @@ export function makeQuestion(
   );
 
   return question;
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {},
+  ): Promise<Question> {
+    const question = makeQuestion(data);
+
+    const { authorId, content, slug, title } =
+      PrismaQuestionMapper.toPrisma(question);
+    await this.prisma.question.create({
+      data: {
+        slug,
+        content,
+        title,
+        authorId,
+      },
+    });
+
+    return question;
+  }
 }
