82ed9cd6f939aae993edec9a4e7809f47b743c2b
Douglas R <douglas.srochak@gmail.com>
2024-03-24 14:58:40 -0300

    chore: Add EditQuestionController for handling question edits

---
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -0,0 +1,54 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { CurrentUser } from '@/infra/auth/current-user.decorator';
+
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
+import { UserPayload } from '@/infra/auth/jwt.strategy';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe';
+import { z } from 'zod';
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases';
+
+const editQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+type EditQuestionBodySchema = z.infer<typeof editQuestionBodySchema>;
+
+@Controller('/questions/:id')
+@UseGuards(JwtAuthGuard)
+export class EditQuestionController {
+  constructor(private editQuestion: EditQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Param('id') questionId: string,
+    @Body(new ZodValidationPipe(editQuestionBodySchema))
+    body: EditQuestionBodySchema,
+    @CurrentUser()
+    user: UserPayload,
+  ) {
+    const { title, content } = body;
+    const userId = user.sub;
+
+    const result = await this.editQuestion.execute({
+      questionId: questionId,
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    return { question: result.value.question };
+  }
+}
